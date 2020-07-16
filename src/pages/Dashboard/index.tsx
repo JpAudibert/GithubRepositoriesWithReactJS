776a046f330706d66995589a7bdeca2bd8c30155
Joao Pedro Basso Audibert <hotsjoao@gmail.com>
2020-07-16 03:30:46 -0300

    Dashboard css

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,58 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoSvg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositorios no Github</Title>;
+  return (
+    <>
+      <img src={logoSvg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github.</Title>
+
+      <Form>
+        <input type="text" placeholder="Digite o nome do Repo" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/40406325?s=460&u=2546cc90394937a3217e099eab030e713c8eaa6b&v=4"
+            alt="Profile"
+          />
+          <div>
+            <strong>JpAudibert/superheroes</strong>
+            <p>Yay super heroes</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/40406325?s=460&u=2546cc90394937a3217e099eab030e713c8eaa6b&v=4"
+            alt="Profile"
+          />
+          <div>
+            <strong>JpAudibert/superheroes</strong>
+            <p>Yay super heroes</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/40406325?s=460&u=2546cc90394937a3217e099eab030e713c8eaa6b&v=4"
+            alt="Profile"
+          />
+          <div>
+            <strong>JpAudibert/superheroes</strong>
+            <p>Yay super heroes</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
