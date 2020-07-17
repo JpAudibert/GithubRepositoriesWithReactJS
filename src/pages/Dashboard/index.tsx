8978d2c1b623429e3f628336d8cd4c23df50af20
Joao Pedro Basso Audibert <hotsjoao@gmail.com>
2020-07-17 03:21:01 -0300

    app done

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,55 +1,99 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 import logoSvg from '../../assets/logo.svg';
 
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (storedRepositories) {
+      return JSON.parse(storedRepositories);
+    }
+
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    if (!newRepo) {
+      setInputError('Digite autor/nome do repo');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca por esse repo');
+    }
+  }
+
   return (
     <>
       <img src={logoSvg} alt="Github Explorer" />
       <Title>Explore repositórios no Github.</Title>
 
-      <Form>
-        <input type="text" placeholder="Digite o nome do Repo" />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          type="text"
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do Repo"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/40406325?s=460&u=2546cc90394937a3217e099eab030e713c8eaa6b&v=4"
-            alt="Profile"
-          />
-          <div>
-            <strong>JpAudibert/superheroes</strong>
-            <p>Yay super heroes</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/40406325?s=460&u=2546cc90394937a3217e099eab030e713c8eaa6b&v=4"
-            alt="Profile"
-          />
-          <div>
-            <strong>JpAudibert/superheroes</strong>
-            <p>Yay super heroes</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/40406325?s=460&u=2546cc90394937a3217e099eab030e713c8eaa6b&v=4"
-            alt="Profile"
-          />
-          <div>
-            <strong>JpAudibert/superheroes</strong>
-            <p>Yay super heroes</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map(repository => (
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
       </Repositories>
     </>
   );
